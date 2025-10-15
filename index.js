721179a028ddfaa97bbe6e5b6bd18813f1598ca2
muhammadjawad-prog <muhammadjawad@cuilahore.edu.pk>
2025-10-15 14:24:52 +0000

    variables & types

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("hello");
+
+// x = 10
+// var y = 20
+
+// let first = "12"
+// let second = 12
+// let result = first === second
+
+// console.log(result);
+// console.log(typeof result);
+
+let balance  = 100
+balance += 200;
+balance--;
+console.log(balance);
